e4c91c8354130c9aeb36564e7eb2f85825c8c20e
Matheus da Silva <matheu.lpp@gmail.com>
2020-09-16 22:49:25 -0300

    Criando pagina Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './App.css';
 import Header from './Components/Header';
+import Home from './Pages/Home';
 
 
 function App() {
   return (
     <div>
         <Header/>
+        <Home/>
    </div>
   );
 }
---
+++ front-web/src/Pages/Home/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { ReactComponent as ArrowIcon } from '../../assets/arrow.svg';
+import { ReactComponent as GamerImg } from '../../assets/gamer.svg';
+
+import './styles.css';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <div className="home-text">
+                <h1 className="home-text-title">
+                    Quais jogos a galera gosta mais?
+                </h1>
+                <h3 className="home-text-subtitle">
+                     Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo! 
+                </h3>
+                <div className="home-actions">
+                    <button className="home-btn">
+                         QUERO SABER QUAIS SÃO
+                    </button>
+                    <div className="home-btn-icon">
+                            <ArrowIcon/>
+                    </div>
+                </div>
+            </div>
+            <GamerImg className="home-img"/>
+        </div>
+    );
+}
+
+export default Home;

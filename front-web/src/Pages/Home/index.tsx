d93aeaf588b5d3e6f9a262f0069b74ded717bdb2
Matheus da Silva <matheu.lpp@gmail.com>
2020-09-20 00:14:09 -0300

    teste

--- front-web/src/Pages/Home/index.tsx
+++ front-web/src/Pages/Home/index.tsx
@@ -11,7 +11,7 @@ const Home = () => {
         <div className="home-container">
             <div className="home-text">
                 <h1 className="home-text-title">
-                    Quais jogos a galera gosta mais??
+                    Quais jogos a galera gosta mais?
                 </h1>
                 <h3 className="home-text-subtitle">
                      Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo! 

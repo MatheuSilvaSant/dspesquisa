0f89702aaae99832de4949aebb204e3a30b11b32
Matheus da Silva <matheu.lpp@gmail.com>
2020-09-20 01:03:00 -0300

    Configurando front-web para produção

--- front-web/src/Pages/Charts/index.tsx
+++ front-web/src/Pages/Charts/index.tsx
@@ -24,7 +24,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://sds1-matheusilva.herokuapp.com';
 
 const Charts = () => {
 
--- front-web/src/Pages/Records/index.tsx
+++ front-web/src/Pages/Records/index.tsx
@@ -10,7 +10,7 @@ import './styles.css';
 
 
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://sds1-matheusilva.herokuapp.com';
 
 const Records = () => {
 
